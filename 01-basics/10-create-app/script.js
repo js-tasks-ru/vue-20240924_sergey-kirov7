e3223f16fa8a337c8a9c1f40f2a41bff1a4ce2f2
Sergey Kirov <s.kirov@bars.group>
2024-10-01 17:00:52 +0300

    Основы: Часть 1 - createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,26 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp } from 'vue';
+
+
+const App = defineComponent({
+    name: 'App',
+    setup() {
+        const event = new Date();
+
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        };
+
+        const currentDay = event.toLocaleDateString(navigator.language, options);
+
+        return {
+            currentDay,
+        }
+    },
+    template: `<div class="container">Сегодня {{currentDay}}</div>`,
+});
+
+const app = createApp(App);
+
+app.mount('#app');
