9ee20c9c7d9ca395bfe5960db06932d5cc267019
ElKlaus <s.kirov@bars.group>
2024-10-23 14:59:29 +0300

    Основы: Часть 2 - Calculator

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,32 @@
-import { defineComponent, ref, computed, watch } from 'vue/dist/vue.esm-bundler'
+import { defineComponent, ref, computed, watchEffect } from 'vue/dist/vue.esm-bundler'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
   setup() {
+    const result = ref(0);
     const fistOperand = ref(0);
     const secondOperand = ref(0);
     const checked = ref("");
 
-    let result = computed(() => {
-      return checked === "sum" ? fistOperand.value + fistOperand.value : fistOperand.value;
-      // switch(checked) {
-      //   case 'sum':
-      //     return fistOperand.value + secondOperand.value
 
-      //   default:
-      //     return 999;
-      // }
-    });
-
-    watch(result, (result) => {
-      console.log(result);
+    watchEffect(() => {
+      switch (checked.value) {
+        case "sum":
+          result.value = fistOperand.value + secondOperand.value;
+          break;
+        case "subtract":
+          result.value = fistOperand.value - secondOperand.value;
+          break;
+        case "multiply":
+          result.value = fistOperand.value * secondOperand.value;
+          break;
+        case "divide":
+          result.value = fistOperand.value / secondOperand.value;
+          break;
+        default:
+          break;
+      }
     })
 
     return {
@@ -36,10 +42,10 @@ export default defineComponent({
       <input type="number" aria-label="First operand" v-model.lazy="fistOperand" />
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" v-model.lazy="checked" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" v-model.lazy="checked" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" v-model.lazy="checked" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" v-model.lazy="checked" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" v-model="checked" value="sum"/>➕</label>
+        <label><input type="radio" name="operator" v-model="checked" value="subtract"/>➖</label>
+        <label><input type="radio" name="operator" v-model="checked" value="multiply"/>✖</label>
+        <label><input type="radio" name="operator" v-model="checked" value="divide"/>➗</label>
       </div>
 
       <input type="number" aria-label="Second operand" v-model.lazy="secondOperand" />
